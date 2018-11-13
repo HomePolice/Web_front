8a107bdbe7ee27f9c07742a5603f88bd83bfc482
sangwonKim <llllssss94@naver.com>
2018-11-13 14:03:44 +0900

    add database connection for geo, box plot

--- homepolice/src/components/main/article/Detail.vue
+++ homepolice/src/components/main/article/Detail.vue
@@ -96,11 +96,14 @@ export default {
     })
     .catch(e => {console.log(e)})
 
+    var most5 = []
+    var val = []
+
     function barChart(){
       var trace1 = {
-        y: [0.75, 5.25, 5.5, 6, 6.2, 6.6, 6.80, 7.0, 7.2, 7.5, 7.5, 7.75, 8.15, 8.15, 8.65, 8.93, 9.2, 9.5, 10, 10.25, 11.5, 12, 16, 20.90, 22.3, 23.25],
+        y: val[0],
         type: 'box',
-        name: '213.17.0.45',
+        name: most5[0],
         marker: {
           color: 'rgb(8,81,156)',
           outliercolor: 'rgba(219, 64, 82, 0.6)',
@@ -113,9 +116,9 @@ export default {
       };
 
       var trace2 = {
-        y: [0.75, 5.25, 5.5, 6, 6.2, 6.6, 6.80, 7.0, 7.2, 7.5, 7.5, 7.75, 8.15, 8.15, 8.65, 8.93, 9.2, 9.5, 10, 10.25, 11.5, 12, 16, 20.90, 22.3, 23.25],
+        y: val[1],
         type: 'box',
-        name: '114.114.114.114',
+        name: most5[1],
         marker: {
           color: 'rgb(8,81,156)',
           outliercolor: 'rgba(219, 64, 82, 0.6)',
@@ -128,9 +131,9 @@ export default {
       };
           
       var trace3 = {
-        y: [0.75, 5.25, 5.5, 6, 6.2, 6.6, 6.80, 7.0, 7.2, 7.5, 7.5, 7.75, 8.15, 8.15, 8.65, 8.93, 9.2, 9.5, 10, 10.25, 11.5, 12, 16, 20.90, 22.3, 23.25],
+        y: val[2],
         type: 'box',
-        name: '2.2.2.2',
+        name: most5[2],
         marker: {
           color: 'rgb(8,81,156)',
           outliercolor: 'rgba(219, 64, 82, 0.6)',
@@ -143,9 +146,9 @@ export default {
       };
           
       var trace4 = {
-        y: [0.75, 5.25, 5.5, 6, 6.2, 6.6, 6.80, 7.0, 7.2, 7.5, 7.5, 7.75, 8.15, 8.15, 8.65, 8.93, 9.2, 9.5, 10, 10.25, 11.5, 12, 16, 20.90, 22.3, 23.25],
+        y: val[3],
         type: 'box',
-        name: '1.1.1.1',
+        name: most5[3],
         marker: {
           color: 'rgb(8,81,156)',
           outliercolor: 'rgba(219, 64, 82, 0.6)',
@@ -156,105 +159,122 @@ export default {
         },
         boxpoints: 'suspectedoutliers'
       };
-        
-      var data = [trace1, trace2, trace3, trace4];
+
+      var trace5 = {
+        y: val[4],
+        type: 'box',
+        name: most5[4],
+        marker: {
+          color: 'rgb(8,81,156)',
+          outliercolor: 'rgba(219, 64, 82, 0.6)',
+          line: {
+            outliercolor: 'rgba(219, 64, 82, 1.0)',
+            outlierwidth: 2
+          }
+        },
+        boxpoints: 'suspectedoutliers'
+      };
+
+      var data = [trace1, trace2, trace3, trace4, trace5];
           
       var layout = {
-        title: 'Box Plot Styling Outliers'
+        title: 'Most 5 IP'
       };
           
       Plotly.newPlot('popChart', data, layout);
     }
 
-    var most5 = []
-
     axios.post("http://127.0.0.1:3000/data/getMost5", {account: 'test'})
     .then(response => {
-      console.log(response.data.ip);
-      console.log(response.data.val);
-      barChart();
+      most5 = response.data.ip;
+
+      for(let i = 0; i < most5.length; i++){
+        axios.post("http://127.0.0.1:3000/data/getListByIp", {account: 'test', ip: most5[i]})
+        .then(res => {
+          val.push(res.data["ip"])
+          if(i == most5.length - 1){
+            barChart();
+          }
+        })
+        .catch(err => console.log(err))
+      }
     })
     .catch(error => console.log(error))
+    
+    var location = []
+    var packetCount = []
 
-    try{
-      
-
-      // This is zoomchart part, not working
-      function geoChart(){
-        var options = {
-          area: { height: 700},
-          container: document.getElementById("geoChart"),
-          data: {
-            preloaded: {
-              nodes: [
-                // 노드 - 위도 경도를 다 찍어야한다
-                { id: "Squaw Valley", coordinates: [-119.181449, 36.707146] }, // long, lat
-                { id: "Atlanta", coordinates: [-84.388846, 33.752504] },
-                { id: "New York", coordinates: [-73.996705, 40.74838] },
-                { id: "Lake Placid", coordinates: [-81.364918, 27.294474] }
-              ],
-              links: [
-                // 링크
-                { from: "New York", to: "Atlanta", drivingTime: "13 hours 3 mins" },
-                { from: "New York", to: "Squaw Valley", drivingTime: "1 day 18 hours" },
-                { from: "New York", to: "Lake Placid", drivingTime: "17 hours 33 mins" },
-                { from: "Lake Placid", to: "Squaw Valley", drivingTime: "1 day 15 hours" },
-                { from: "Atlanta", to: "Squaw Valley", drivingTime: "1 day 10 hours" }
-              ]
-            }
-          },
-          events: {
-            // 클릭 이벤트! 누르면 정보를 띄울수 있을것으로 생각됨
-            onClick: function (event, args) {
-              if (args.clickNode) alert("You clicked on " + args.clickNode.data.id + ".");
-              if (args.clickLink) alert("You clicked on a link.");
-            }
-          },
-          layers: [
-            {
-              // 스타일을 결정하는 부분 입니다.
-              name: "Points",
-              type: "items",
-              style: {
-                nodeStyleFunction: function (node) {
-                  node.label = node.data.id;
-                },
-                linkStyleFunction: function (link) {
-                  link.label = link.data.drivingTime;
-                },
-                node: {
-                  fillColor: "#09c",
-                  lineColor: "#07a",
-                  lineWidth: 2
-                },
-                nodeLabel: {
-                  backgroundStyle: { fillColor: "black" },
-                  textStyle: { fillColor: "white" }
-                },
-                linkLabel: {
-                  backgroundStyle: { fillColor: "rgba(0, 0, 0, 0.6)" },
-                  textStyle: { fillColor: "white" }
-                }
+    // This is zoomchart part, not working
+    function geoChart(){
+      var options = {
+        area: { height: 700},
+        container: document.getElementById("geoChart"),
+        data: {
+          preloaded: {
+            nodes: location,
+            links: packetCount
+          }
+        },
+        events: {
+          // 클릭 이벤트! 누르면 정보를 띄울수 있을것으로 생각됨
+          onClick: function (event, args) {
+            if (args.clickNode) alert("You clicked on " + args.clickNode.data.id + ".");
+            if (args.clickLink) alert("You clicked on a link.");
+          }
+        },
+        layers: [
+          {
+            // 스타일을 결정하는 부분 입니다.
+            name: "Points",
+            type: "items",
+            style: {
+              nodeStyleFunction: function (node) {
+                node.label = node.data.id;
+              },
+              linkStyleFunction: function (link) {
+                link.label = link.data.traffic;
+              },
+              node: {
+                fillColor: "#09c",
+                lineColor: "#07a",
+                lineWidth: 2
+              },
+              nodeLabel: {
+                backgroundStyle: { fillColor: "black" },
+                textStyle: { fillColor: "white" }
+              },
+              linkLabel: {
+                backgroundStyle: { fillColor: "rgba(0, 0, 0, 0.6)" },
+                textStyle: { fillColor: "white" }
               }
             }
-          ],
-          navigation: {
-            // 처음 로딩되었을때 보일 지도 위치를 위경도로 지정, 기본 줌 상태도 지정
-            initialLat: 35.04409,
-            initialLng: -90.246213,
-            initialZoom: 4,
-            minZoom: 4
-          },  // 더블클릭 불가능 지정
-          advanced: { pointer: { noClickOnDoubleClick: false } }
-        };
-        chart = new GeoChart(options);
-      }
-      
-      geoChart();
-      }
-    catch(e){
-      console.log(e)
+          }
+        ],
+        navigation: {
+          // 처음 로딩되었을때 보일 지도 위치를 위경도로 지정, 기본 줌 상태도 지정
+          initialLat: 37.566536,
+          initialLng: 126.977966,
+          initialZoom: 4,
+          minZoom: 1
+        },  // 더블클릭 불가능 지정
+        advanced: { pointer: { noClickOnDoubleClick: false } }
+      };
+      let chart = new GeoChart(options);
     }
+
+    axios.post("http://127.0.0.1:3000/data/geo", {account: 'test'})
+    .then(response => {
+      let get = response.data
+
+      for(let i = 0; i < get.length; i++){
+        location.push({"id": get[i]["nation"], "coordinates": [126.977966, 37.566536 - (100 * i)]})
+        packetCount.push({"from": "korea", "to": get[i]["nation"], "traffic": get[i]["abnormal_count"]})
+      }
+      console.log(location)
+      console.log(packetCount)
+      geoChart();
+    })
+    .catch(err => console.log(err))
   }
 }
 </script>
