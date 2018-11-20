5e8798e8530d4d294ebe275769aae88454c6f605
sangwonKim <llllssss94@naver.com>
2018-11-20 16:44:02 +0900

    insert public ip

--- homepolice/config/index.js
+++ homepolice/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: { },
 
     // Various Dev Server settings
-    public: "0.0.0.0", // can be overwritten by process.env.HOST
+    host: "13.209.93.63", // can be overwritten by process.env.HOST
     disableHostCheck: true,
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
