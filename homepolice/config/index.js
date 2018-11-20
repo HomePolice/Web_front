9348368afb8a4835c244c99ddbd31f8f3ff823a2
sangwonKim <llllssss94@naver.com>
2018-11-20 17:00:55 +0900

    add devserver options

--- homepolice/config/index.js
+++ homepolice/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: { },
 
     // Various Dev Server settings
-    host: "13.209.93.63", // can be overwritten by process.env.HOST
+    host: "0.0.0.0", // can be overwritten by process.env.HOST
     disableHostCheck: true,
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
@@ -42,6 +42,12 @@ module.exports = {
     cacheBusting: true,
 
     cssSourceMap: true
+  },
+  
+  devServer: {
+    host: '0.0.0.0',
+    hot: true,
+    disableHostCheck: true,
   },
 
   build: {
