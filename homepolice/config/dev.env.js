7da1de6c0227f0368f2270297aff5b509f17de3a
sangwonKim <llllssss94@naver.com>
2018-11-20 16:50:07 +0900

    public access grant

--- homepolice/config/dev.env.js
+++ homepolice/config/dev.env.js
@@ -3,5 +3,8 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  host: "13.209.93.63",
+    disableHostCheck: true,
+    port: 8080
 })
